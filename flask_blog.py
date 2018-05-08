ab088c22bcd024a97f13dc6ddeff296921afb3ba
Aditya7861 <aditya9640@gmail.com>
2018-05-08 09:42:06 +0530

    Adding header

--- flask_blog.py
+++ flask_blog.py
@@ -1,12 +1,14 @@
-from flask import Flask , render_template
-app = Flask(__name__)
+from flask import Flask , render_template , url_for
+from form import RegistrationForm, LoginForm
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '33e691769378c0ad911e5a951faccdf8'
 posts = [
     {
     'author': "Aditya Singh",
     'title': "First Blog Post",
     'content': 'First Blog Post Content'
-}, 
+},
 {
     'author': "Pradeep Gupta",
     'title': "First Pradeep Post",
@@ -25,6 +27,18 @@ def hello():
 @app.route("/about")
 def about():
     return render_template('about.html',title = "About")
+
+
+@app.route("/registration")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title='register', form = form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='login', form=form)
 
 
 if __name__ == "__main__":

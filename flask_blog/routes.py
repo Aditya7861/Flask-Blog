c957220a60d74bfd95f653d44e09b5c278048444
Aditya7861 <aditya9640@gmail.com>
2018-06-11 19:22:32 +0530

    Added post update Feature

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -92,7 +92,7 @@ def account():
         form.username.data = current_user.username
         form.email.data = current_user.email
     image_file = url_for('static',filename='profile_pic/'+current_user.image_file)
-    return render_template('account.html',title = 'Account',image_file=image_file,form=form)
+    return render_template('account.html',title = 'Account',image_file=image_file,form=form , legend = "create Post")
 
 
 @app.route("/post/new", methods=['GET', 'POST'])
@@ -118,12 +118,21 @@ def post(post_id):
 
 
 
-@app.route("/post/<int:post_id>/update")
+@app.route("/post/<int:post_id>/update",methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
         abort(403)
     form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your Post has been updated","success")
+        return redirect(url_for('post',post_id=post.id))
+    elif request.method == "GET":
+        form.title.data  = post.title
+        form.content.data = post.content
     return render_template('create_post.html', title='Update post', form=form, legend='Update Post')
 
